95f2c4275d7c88d08fc603e18c4af9f96670bc7d
pentem <martijn.pentenga@movares.nl>
2022-10-03 13:32:05 +0200

    version/bumpversion changes

--- shapelyM/__init__.py
+++ shapelyM/__init__.py
@@ -2,7 +2,7 @@
 
 from typing import Any, List
 
-__version__ = "1.beta4"
+__version__ = "0.0.1-dev5123"
 
 from shapelyM.lineString import LineStringMeasure
 from shapelyM.measurePoint import MeasurePoint
