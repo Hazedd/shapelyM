25083cb7b13e5c50ffa3c5a0cd6d6ba945307b55
pentem <martijn.pentenga@movares.nl>
2022-10-03 13:34:42 +0200

    Bump version: 0.0.1.gamma2 → 0.0.1.gamma3

--- shapelyM/__init__.py
+++ shapelyM/__init__.py
@@ -2,7 +2,7 @@
 
 from typing import Any, List
 
-__version__ = "0.0.1.gamma2"
+__version__ = "0.0.1.gamma3"
 
 from shapelyM.lineString import LineStringMeasure
 from shapelyM.measurePoint import MeasurePoint
