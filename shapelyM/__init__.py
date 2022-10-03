20cf67c955fbd79f21a08d6551c998cece3e8a10
pentem <martijn.pentenga@movares.nl>
2022-10-03 23:12:10 +0200

    added more stuff to README.md

--- shapelyM/__init__.py
+++ shapelyM/__init__.py
@@ -1,10 +1,10 @@
-"""Ready-to-use package for IMX insights."""
-
 from typing import Any, List
 
 __version__ = "0.0.3-alpha3"
 
 from shapelyM.lineString import LineStringMeasure
 from shapelyM.measurePoint import MeasurePoint
+
+# todo: expose projection response
 
 __all__: List[Any] = ["MeasurePoint", "LineStringMeasure"]
