25837185e791ec55e76b44bcb6491eb005d9a04e
pentem <martijn.pentenga@movares.nl>
2022-10-03 13:49:04 +0200

    Bump version: 0.0.1.dev → 0.0.1.alpha

--- shapelyM/__init__.py
+++ shapelyM/__init__.py
@@ -2,7 +2,7 @@
 
 from typing import Any, List
 
-__version__ = "0.0.1.dev"
+__version__ = "0.0.1.alpha"
 
 from shapelyM.lineString import LineStringMeasure
 from shapelyM.measurePoint import MeasurePoint
