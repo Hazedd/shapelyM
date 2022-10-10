e0a0c148c3c15e5ecdee3046c8c6c640348d536e
Hazedd <Hazedd@users.noreply.github.com>
2022-10-10 18:30:32 +0000

    Bump version: 0.0.5-dev2 → 0.0.5-dev3

--- shapelyM/__init__.py
+++ shapelyM/__init__.py
@@ -1,6 +1,6 @@
 from typing import Any, List
 
-__version__ = "0.0.5-dev2"
+__version__ = "0.0.5-dev3"
 
 from shapelyM.lineString import LineStringMeasure
 from shapelyM.measurePoint import MeasurePoint
