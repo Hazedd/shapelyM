19e61e3894852dc59a84bdcbb2a93a43344e4c9d
pentem <martijn.pentenga@movares.nl>
2022-10-12 22:50:38 +0200

    bit refactor

--- shapelyM/__init__.py
+++ shapelyM/__init__.py
@@ -1,6 +1,6 @@
 from typing import Any, List
 
-__version__ = "0.0.5-dev4"
+__version__ = "0.0.5-dev5"
 
 from shapelyM.lineString import LineStringMeasure
 from shapelyM.measurePoint import MeasurePoint
