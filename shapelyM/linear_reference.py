56381d67a0526810ac3947cf3bbd97c6e51b34ad
pentem <martijn.pentenga@movares.nl>
2022-10-12 22:13:18 +0200

    bit refactor

--- shapelyM/linear_reference.py
+++ shapelyM/linear_reference.py
@@ -4,6 +4,28 @@ from shapelyM.helpers import check_point_between_points, project_point_on_line, 
 from shapelyM.measurePoint import MeasurePoint
 
 
+def _correct_overshoot(
+    point_1: PointProtocol, point_2: PointProtocol, point_on_line_2d: PointProtocol
+) -> MinimalPoint:
+    # if not between we assume it overshoots
+    if not check_point_between_points(point_1, point_2, point_on_line_2d):
+        point_on_line_2d = MinimalPoint(point_2.x, point_2.y)
+    return point_on_line_2d
+
+
+def _set_z_on_percentage(point_1, point_2, point_on_line_2d) -> MeasurePoint:
+    point = MeasurePoint(point_on_line_2d.x, point_on_line_2d.y)
+    start_distance = point.distance(point_1, force_2d=True)
+    end_distance = point_1.distance(point_2, force_2d=True)
+    try:
+        percentage = start_distance / end_distance
+    except ZeroDivisionError:
+        percentage = 0
+
+    point.z = point_1.z + ((point_2.z - point_1.z) * percentage)
+    return point
+
+
 def linear_reference_point_on_line(
     point_1: PointProtocol, point_2: PointProtocol, point_to_project: PointProtocol
 ) -> MeasurePoint:
@@ -17,27 +39,11 @@ def linear_reference_point_on_line(
 
     if point_1.z is not None and point_2.z is not None and point_to_project.z is not None:
         point_on_line_2d = project_point_on_line(point_1, point_2, point_to_project)
-
-        # if not between we assume it overshoots
-        if not check_point_between_points(point_1, point_2, point_on_line_2d):
-            point_on_line_2d = MinimalPoint(point_2.x, point_2.y)
-
-        point = MeasurePoint(point_on_line_2d.x, point_on_line_2d.y)
-        start_distance = point.distance(point_1, force_2d=True)
-        end_distance = point_1.distance(point_2, force_2d=True)
-        try:
-            percentage = start_distance / end_distance
-        except ZeroDivisionError:
-            percentage = 0
-
-        point.z = point_1.z + ((point_2.z - point_1.z) * percentage)
-        return project_point_on_line(point_1, point_2, point).coords
+        point_on_line_2d = _correct_overshoot(point_1, point_2, point_on_line_2d)
+        point_on_line_3d = _set_z_on_percentage(point_1, point_2, point_on_line_2d)
+        return point_on_line_3d
 
     else:
-        point_on_line_2d = project_point_on_line(point_1, point_2, point_to_project).coords
-
-        # if not between we assume it overshoots
-        if not check_point_between_points(point_1, point_2, MeasurePoint(*point_on_line_2d)):
-            point_on_line_2d = [point_2.x, point_2.y]
-
+        point_on_line_2d = project_point_on_line(point_1, point_2, point_to_project)
+        point_on_line_2d = _correct_overshoot(point_1, point_2, point_on_line_2d)
         return point_on_line_2d
