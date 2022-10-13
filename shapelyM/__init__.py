fe26dfc2a355aa31933fc4621fa4262058da00df
Hazedd <Hazedd@users.noreply.github.com>
2022-10-13 17:22:21 +0000

    Bump version: 0.0.5-dev6 → 0.0.5-dev7

--- shapelyM/__init__.py
+++ shapelyM/__init__.py
@@ -1,6 +1,6 @@
 from typing import Any, List
 
-__version__ = "0.0.5-dev6"
+__version__ = "0.0.5-dev7"
 
 from shapelyM.linear_reference import LineProjection
 from shapelyM.measureLineString import MeasureLineString
